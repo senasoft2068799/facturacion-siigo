3bc010a6b2a9a22c162c39f1922501f0134bd0e9
Ju4nPx <juanpmachadob@gmail.com>
2021-10-01 15:09:14 -0500

    Traduccion mensajes a español

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -1,22 +1,9 @@
 <?php
-
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
-
+    'reset' => '¡Su contraseña ha sido restablecida!',
+    'sent' => '¡Recordatorio de contraseña enviado!',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
+    'throttled' => 'Por favor espere antes de volver a intentarlo.',
+    'password' => 'Las contraseñas deben tener al menos seis caracteres y coincidir con la confirmación.'
 ];
